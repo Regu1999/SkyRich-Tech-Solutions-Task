bc84b65575bcceaa0c997119ea8ad14b364e494f
Regu1999 <reguramachandran007@gmail.com>
2025-01-30 13:30:35 +0530

    path error fixed

--- src/pages/CompletedChallenges.jsx
+++ src/pages/CompletedChallenges.jsx
@@ -1,7 +1,7 @@
 import { motion, AnimatePresence } from "motion/react"
 import { useContext } from "react";
 
-import ChallengeInfo from "../component/ChallengeInfo";
+import ChallengeInfo from "../Component/ChallengeInfo";
 import ChallengesContext from "../Component/context/ChallengesContext";
 import { challengeFilter } from "../utlity/challengesFilter"
 import FileNotFound from "../Component/UI/FileNotFound";
