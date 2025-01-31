2a9fd4890e235946533519e264f0d84480c62db3
Regu1999 <reguramachandran007@gmail.com>
2025-01-31 11:53:18 +0530

    Info component animation changed

--- src/Component/UI/Info.jsx
+++ src/Component/UI/Info.jsx
@@ -1,10 +1,10 @@
 import { motion } from "motion/react"
 const Info = ({ children, title }) => {
     return <motion.section
-        initial={{ y: '20%', opacity: 0 }}
+        initial={{ y: '10%', opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
         viewport={{ once: true }}
-        transition={{ type: "spring", delay: .2 }}
+        transition={{ type: "spring", delay: .1 }}
     >
         <h3 style={{textAlign:'center', padding:'2rem'}}>
             {title}
