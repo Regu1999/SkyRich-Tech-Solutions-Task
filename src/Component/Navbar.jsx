9f6ad5cd4065e9ce2e16816cf2fff2ec2f94d810
Regu1999 <reguramachandran007@gmail.com>
2025-01-31 11:39:29 +0530

    navbar color animation changes

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,24 +1,22 @@
-import { motion } from "motion/react";
+import { motion, useScroll, useTransform } from "motion/react";
 import { Link, useLocation } from "react-router"
 import "../assets/styles/navbar.css"
 import superHero from "/images/superhero.jpg"
 
 const Navbar = () => {
-const location=useLocation();
-console.log(location.pathname);
-
-if (location.pathname!=="/") {
-    console.log("hi");
-    
-}
+    const location = useLocation();
+    const { scrollYProgress } = useScroll()
+    const navBgColor = useTransform(scrollYProgress, [0, .5, 1], ["","" ,'#111d32'])
     return <motion.nav style={{
-        background:location.pathname!=="/"?"linear-gradient(#222c31, #111d32)":''
-    }}>
+        // backgroundColor: navBgColor
+        background: location.pathname !== "/" ? "linear-gradient(#222c31, #111d32)" : navBgColor
+    }}
+    >
 
         <div className="logo"><Link to="/"><img src={superHero} alt="super man" /></Link></div>
         <div>
             <Link to="/challenges">Challenges</Link>
-{/*             <Link to="/movie">Movies list</Link> */}
+            {/*             <Link to="/movie">Movies list</Link> */}
         </div>
     </motion.nav>
 }
